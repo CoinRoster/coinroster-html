bdaca5bfa63aef041dbdf1d1d8ee4bbced5dc99e
gov <govi218mu@gmail.com>
2018-05-30 12:17:33 -0400

    more merges

--- admin/js/error.js
+++ admin/js/error.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
     window.onerror = function (message, url, lineNo) 
         {
         alert('Error: ' + message + 
               '\nUrl: ' + url + 
               '\nLine Number: ' + lineNo);
         return true;   
-=======
-    window.onerror = function (message, url, lineNo) 
-        {
-        alert('Error: ' + message + 
-              '\nUrl: ' + url + 
-              '\nLine Number: ' + lineNo);
-        return true;   
->>>>>>> origin/live
         };
