7101a8a32504b991a231a2a0b46ace4d22463f0c
gov <govi218mu@gmail.com>
2018-06-12 13:47:22 -0400

    logs

--- admin/js/promos.js
+++ admin/js/promos.js
@@ -325,6 +325,7 @@
         }
         
         if(id("create_promo__referrer_selector") === null) {
+            console.log(id("user_affiliate_id").value);
             referrer =  id("user_affiliate_id").value;
         } else {
             referrer = selectorValue("create_promo__referrer_selector");
