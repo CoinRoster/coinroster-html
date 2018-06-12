987e8da9f4ed4e52b5d94adbdf7993a1ea83c478
gov <govi218mu@gmail.com>
2018-06-12 10:34:01 -0400

    added user generated promos

--- admin/js/promos.js
+++ admin/js/promos.js
@@ -305,10 +305,10 @@
         var 
         
         promo_code = id("create_promo__code_input").value.trim(),
-        description = id("create_promo__description_input").value,
-        referrer = selectorValue("create_promo__referrer_selector"),
+        description = id("create_promo__description_input").value || "User Generated Promo Code",
+        referrer = selectorValue("create_promo__referrer_selector") || id(user_affiliate_id).value,
         free_play_amount = id("create_promo__freeplay_amount_input").value,
-        rollover_multiple = id("create_promo__rollover_input").value,
+        rollover_multiple = id("create_promo__rollover_input").value || "5",
         max_use = id("create_promo__max_use_input").value,
         expires_input_value = id("create_promo__deadline_tcal").value,
         expires_date = dateconv_date_start_time(id("create_promo__deadline_tcal").date_ms),
