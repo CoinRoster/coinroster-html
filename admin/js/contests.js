e64e88d84919c77a03f2680782237cc62aa43b78
gov <govi218mu@gmail.com>
2018-07-01 16:00:31 -0400

    fixed pane bug

--- admin/js/contests.js
+++ admin/js/contests.js
@@ -2040,3 +2040,157 @@
             window.location.reload();
         })
     }
+
+    window.contest_obj = [];
+    function populate_user_contest() {
+        var call = api({ method: "GetUnapprovedContests", args: {settlement_type:'crowd'} });
+        
+        if (call.status === "1") {
+            
+            var
+            
+            pending_contest_array = call.pending_contests,
+            number_of_contests = pending_contest_array.length,
+            table = new_table("user_contest_table"),
+            row_count = 0;
+            table.border = "1";
+
+            if (number_of_contests > 0) {
+                for (var i = 0; i < number_of_contests; i++) {
+                    var contest_item = pending_contest_array[i],
+
+                    contest_id = contest_item.contest_id,
+                    created = contest_item.created,
+                    created_date = dateconv_ms_to_string(created),
+                    created_time = dateconv_ms_to_time(created),
+                    created_by = contest_item.created_by,
+                    category = contest_item.category,
+                    title = contest_item.title,
+                    description = contest_item.description,
+                    settlement_type = contest_item.settlement_type,
+                    option_table = contest_item.option_table,
+                    rake = contest_item.rake,
+                    cost_per_entry = contest_item.cost_per_entry;
+                    
+                    window.contest_obj[contest_id] = {
+                        contest_id: contest_id,
+                        created_date: created_date,
+                        created_time: created_time,
+                        created_by: created_by,
+                        category: category,
+                        title: title,
+                        description: description,
+                        settlement_type: settlement_type,
+                        option_table: option_table,
+                        rake: rake,
+                        cost_per_entry: cost_per_entry,
+                    };
+
+                    var row = new_row(table, row_count++, [
+                        contest_id,
+                        created_date,
+                        created_time,
+                        created_by,
+                        category,
+                        title,
+                        description,
+                        settlement_type,
+                        option_table,
+                        rake,
+                        cost_per_entry ,
+                        "<button onclick=\"settle_contest(" + contest_id + ")\">Settle Contest</button>"
+                    ]);
+
+                    row[1].style.textAlign = "right";
+                    row[6].style.textAlign = "right";
+                    
+                    // highlight if older than 30 days:
+                    
+                    if (new Date().getTime()-30*1440*60*1000 > created) row[2].style.background = "rgb(255,231,166)";
+                }
+                
+                var header = new_row(table, 0, [
+                    "#",
+                    "Date",
+                    "Time",
+                    "Created By",
+                    "Category",
+                    "Title",
+                    "Description",
+                    "Settlement Type",
+                    "Option Table",
+                    "Rake",
+                    "Cost Per Entry",
+                    "Settle"
+                ]);
+                // style headers:
+
+                for (var i=1; i<header.length; i++) header[i].className = "header_cell";
+        
+                header[1].style.textAlign = "center";
+                header[6].style.textAlign = "right";
+            }
+            else id("user_contest_table").innerHTML = "None";
+        }
+        else id("user_contest_table").innerHTML = "Error getting transactions";       
+    }
+
+    function settle_contest(contest_id) {
+
+        window.contest_id_to_settle = contest_id;
+
+        var 
+        
+        contest = window.contest_obj[contest_id];
+        contest_type = contest.contest_type,
+        contest_title = contest.title,
+        // scores_updated = contest_item.scores_updated,
+        option_table = JSON.parse(contest.option_table);
+
+        hide("user_contest_table");
+        show("settle_contest");
+
+        id("contest_type").innerHTML = contest_type;
+        id("contest_id").innerHTML = contest_id;
+        id("contest_title").innerHTML = contest_title;
+
+        show("settle_pari_mutuel");
+        
+        var table = new_table("pari_mutuel_outcome_table");
+        
+        for (var i=0; i<option_table.length; i++)
+            {
+            var option_item = option_table[i],
+
+            option_id = option_item.id,
+            description = option_item.description;
+            
+            new_row(table, -1, [
+                "<input type=\"radio\" name=\"pari_mutuel_outcome_radio\" value=\"" + option_id + "\" />",
+                description
+            ]);
+            }
+    }
+
+    function settle_pari_mutuel_contest() {
+
+        var winning_outcome = get_radio_selection("pari_mutuel_outcome_radio");
+        
+        if (winning_outcome === null) return alert("Please select winning outcome!");
+        alert('success');
+        api({
+            method: "UserSettleContest",
+            args: {
+                contest_id: window.contest_id_to_settle,
+                winning_outcome: winning_outcome
+            }
+        }, function(call)
+            {
+            if (call.status === "1") 
+                {
+                alert("Contest has been settled! Reloading panel.");
+                location.reload();
+                }
+            else alert("Error: " + call.error);
+            });
+    }
