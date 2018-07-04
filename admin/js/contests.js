948becd741bb1e2f9adbedacbe12fe33fe58f61b
gov <govi218mu@gmail.com>
2018-07-04 16:08:11 -0400

    log settlement type

--- admin/js/contests.js
+++ admin/js/contests.js
@@ -1202,10 +1202,11 @@
         option_rows = pari_mutuel_table.rows,
         option_table = [];
         
-        if (args.category === 'USERGENERATED') {
-            args.settlement_type = selectorValue('settlement_type_selector');
-        }
+        var settlement_type = selectorValue('settlement_type_selector');
+        if(settlement_type === '') return alert('settlement type error');
+        args.settlement_type = settlement_type;
 
+        
         for (var i=1/*header is 0*/; i<option_rows.length; i++)
             {
             var description = option_rows[i].querySelector("input").value;
