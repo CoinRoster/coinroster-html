80982bcfe1d91a4e9e2b41a52d72bb9155f4ee3f
vadimgavrish <vadim@gavrish.ca>
2018-07-27 01:17:24 -0400

    sport

--- js/create_contest.js
+++ js/create_contest.js
@@ -71,8 +71,6 @@ contest_type_selector.onchange = function()
       var golf = true;
       var baseball = false;
 
-      console.log("hello")
-
        if (basketball) {
         var option = document.createElement("option");
         option.text = "Basketball";
