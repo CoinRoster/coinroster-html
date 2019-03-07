800c7771a0531f6d29c0bf858ff554b569833f3d
Murrr-Andrew <murrr.andrew@gmail.com>
2019-03-07 17:36:43 +0200

    Fixes

--- js/simple_modal_big.js
+++ js/simple_modal_big.js
@@ -5,5 +5,5 @@
     simple_modal_close_big = id("simple_modal_close_big");
 
     function show_simple_modal_big() {
-        .simple_modal_shade_big.classList.toggle("open");
+        simple_modal_shade_big.classList.toggle("open");
     }
