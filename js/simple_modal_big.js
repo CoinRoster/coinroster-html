6d03cf57175ed424d1d7b09e51222ef67e68c5db
Murrr-Andrew <murrr.andrew@gmail.com>
2019-03-07 18:05:30 +0200

    Adding fixes

--- js/simple_modal_big.js
+++ js/simple_modal_big.js
@@ -1,8 +1,8 @@
-    var 
-
-    simple_modal_shade_big = document.getElementById("simple_modal_centering_container_big"),
-    simple_modal_close_big = id("simple_modal_close_big");
+    var simple_modal_shade_big = document.getElementById("simple_modal_centering_container_big");
 
     function show_simple_modal_big() {
         simple_modal_shade_big.classList.add("open");
+    }
+    function close_simple_modal_big() {
+        simple_modal_shade_big.classList.remove("open");
     }
