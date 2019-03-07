26f62ca3788a5cb888bd4d48557a9b3f16a21eb2
Murrr-Andrew <murrr.andrew@gmail.com>
2019-03-07 17:45:36 +0200

    fixes

--- js/simple_modal_big.js
+++ js/simple_modal_big.js
@@ -1,6 +1,6 @@
     var 
 
-    simple_modal_shade_big = document.getElementById("simple_modal_shade_big"),
+    simple_modal_shade_big = document.getElementById("simple_modal_centering_container_big"),
     simple_modal_close_big = id("simple_modal_close_big");
 
     function show_simple_modal_big() {
